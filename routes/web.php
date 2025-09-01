7bd4921bfbb67b04edaef26efba9012cab97f133
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-09-01 18:15:54 +0000

    updated

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'github/'], function() {
+    Route::post('webhooks', function () {
+        try {
+            $secret = "monkey@21";
+            $payload = file_get_contents("php://input");
+            // file_put_contents("webhook_request.log", $payload, FILE_APPEND);
+            $signature = $_SERVER["HTTP_X_HUB_SIGNATURE_256"] ?? "";
+            $hash = "sha256=" . hash_hmac("sha256", $payload, $secret);
+            if (!hash_equals($hash, $signature)) {
+                http_response_code(403);
+                exit("Invalid Signature");
+            }
+
+            $data = json_decode($payload, true);
+            if ($data["ref"] === "refs/heads/main") {
+                exec("cd ~/public_html/webhooks && git pull origin main 2>&1", $output, $returnCode);
+                file_put_contents("webhook.log", implode('\n', $output), FILE_APPEND);
+            }
+            return response()->json('success', 200);
+        } catch (Throwable $e) {
+            return response()->json($e->getMessage(), 500);
+        }
+    });
     Route::post('bibek/portfolio', function () {
         try {
             $secret = "monkey@21";
@@ -24,7 +46,7 @@ Route::group(['prefix' => 'github/'], function() {
                 file_put_contents("webhook.log", implode('\n', $output), FILE_APPEND);
             }
             return response()->json('success', 200);
-        } catch (Exception $e) {
+        } catch (Throwable $e) {
             return response()->json($e->getMessage(), 500);
         }
     });
