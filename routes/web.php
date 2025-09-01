db01f19a6208846f4bb2654f7339298a54aad6d1
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-09-01 20:17:49 +0000

    updated

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,28 @@ Route::group(['prefix' => 'github/'], function() {
             return response()->json($e->getMessage(), 500);
         }
     });
+        Route::post('bibek/portfolio', function () {
+        try {
+            $secret = "monkey@21";
+            $payload = file_get_contents("php://input");
+            // file_put_contents("webhook_request.log", $payload, FILE_APPEND);
+            $signature = $_SERVER["HTTP_X_HUB_SIGNATURE_256"] ?? "";
+            $hash = "sha256=" . hash_hmac("sha256", $payload, $secret);
+            if (!hash_equals($hash, $signature)) {
+                http_response_code(403);
+                exit("Invalid Signature");
+            }
+
+            $data = json_decode($payload, true);
+            if ($data["ref"] === "refs/heads/main") {
+                exec("cd ~/public_html/bibek_portfolio && git pull origin main 2>&1", $output, $returnCode);
+                file_put_contents("webhook.log", implode('\n', $output), FILE_APPEND);
+            }
+            return response()->json('success', 200);
+        } catch (Throwable $e) {
+            return response()->json($e->getMessage(), 500);
+        }
+    });
     Route::post('chatapp', function () {
         try {
             $secret = "monkey@21";
@@ -50,7 +72,7 @@ Route::group(['prefix' => 'github/'], function() {
             return response()->json($e->getMessage(), 500);
         }
     });
-    Route::post('bibek/portfolio', function () {
+    Route::post('foodweb', function () {
         try {
             $secret = "monkey@21";
             $payload = file_get_contents("php://input");
@@ -63,8 +85,8 @@ Route::group(['prefix' => 'github/'], function() {
             }
 
             $data = json_decode($payload, true);
-            if ($data["ref"] === "refs/heads/main") {
-                exec("cd ~/public_html/bibek_portfolio && git pull origin main 2>&1", $output, $returnCode);
+            if ($data["ref"] === "refs/heads/development") {
+                exec("cd ~/public_html/foodweb && git pull origin development 2>&1", $output, $returnCode);
                 file_put_contents("webhook.log", implode('\n', $output), FILE_APPEND);
             }
             return response()->json('success', 200);
