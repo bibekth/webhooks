8b29d3ec70daef8659c6cd8b20ba6f2613fcee75
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-11-13 22:43:08 +0545

    update: made a webhook url for store9nepal

--- routes/web.php
+++ routes/web.php
@@ -116,4 +116,26 @@ Route::group(['prefix' => 'github/'], function() {
             return response()->json($e->getMessage(), 500);
         }
     });
+    Route::post('store9nepal', function () {
+        try {
+            $secret = "monkey@21";
+            $payload = file_get_contents("php://input");
+            // file_put_contents("webhook_request.log", $payload, FILE_APPEND);
+            $signature = $_SERVER["HTTP_X_HUB_SIGNATURE_256"] ?? "";
+            $hash = "sha256=" . hash_hmac("sha256", $payload, $secret);
+            if (!hash_equals($hash, $signature)) {
+                http_response_code(403);
+                exit("Invalid Signature");
+            }
+
+            $data = json_decode($payload, true);
+            if ($data["ref"] === "refs/heads/laravel") {
+                exec("cd ~/public_html/store9nepal && git pull origin laravel 2>&1", $output, $returnCode);
+                file_put_contents("webhook.log", implode('\n', $output), FILE_APPEND);
+            }
+            return response()->json('success', 200);
+        } catch (Throwable $e) {
+            return response()->json($e->getMessage(), 500);
+        }
+    });
 });
